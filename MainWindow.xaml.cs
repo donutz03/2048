0fe36b67f01c472618c548654b3451e9564f13aa
donutz03 <ionuthodoroaga@icloud.com>
2025-02-16 09:45:02 +0200

    intermediate, working on random start position

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -37,7 +37,9 @@ public partial class MainWindow : Window
             RowDefinition rowDef = new RowDefinition();
             myGrid.RowDefinitions.Add(rowDef);
         }
-        
+
+        int[] startPosition = randomPosition();
+        TextBlock txt1 = new TextBlock();
         for (int i = 0; i < 4; i++)
         {
             for (int j = 0; j < 4; j++)
@@ -55,5 +57,59 @@ public partial class MainWindow : Window
         }
 
         this.Content = myGrid;
+    }
+
+    int[] randomPosition()
+    {
+        var columnPosition1 = new Random().Next(4);
+        var rowPosition1 = new Random().Next(4);
+        
+        var columnPosition2 = new Random().Next(4);
+        var rowPosition2 = new Random().Next(4);
+        while (columnPosition1 == columnPosition2 && rowPosition1 == rowPosition2)
+        {
+            columnPosition2 = new Random().Next(4);
+            rowPosition2 = new Random().Next(4);
+        }
+
+        var oddsOfGetting2or4 = Get10PercentOddsFor4InStartingPosition();
+        var randomPosition1 = new Random().Next(100);
+        var randomPosition2 = new Random().Next(100);
+        
+        while (randomPosition1 == randomPosition2)
+        {
+            randomPosition2 = new Random().Next(100);
+        }
+        
+        return new int[] { columnPosition1, columnPosition2, 
+            rowPosition1, rowPosition2, 
+            oddsOfGetting2or4[randomPosition1],
+            oddsOfGetting2or4[randomPosition2] };
+    }
+
+    private static int[] Get10PercentOddsFor4InStartingPosition()
+    {
+        int[] oddsOfGetting2Or4 = new int[100];
+        var count = 0;
+        while (count != 10)
+        {
+            var poz = new Random().Next(100);
+            if (oddsOfGetting2Or4[poz] == 0)
+            {
+                oddsOfGetting2Or4[poz] = 4;
+                count++;
+            }
+            count++;
+        }
+
+        for (int i = 0; i < 100; i++)
+        {
+            if (oddsOfGetting2Or4[i] == 0)
+            {
+                oddsOfGetting2Or4[i] = 2;
+            }
+        }
+
+        return oddsOfGetting2Or4;
     }
 }
