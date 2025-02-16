fde838363ce9aaf23704ddc1aee55f2d6d2f771c
donutz03 <ionuthodoroaga@icloud.com>
2025-02-16 14:13:17 +0200

    added keydown event handling

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,7 +4,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
+using System.Windows.Media; 
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -16,10 +16,13 @@ namespace game2048cs;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private static Grid myGrid;
     public MainWindow()
     {
+        
         InitializeComponent();
-        Grid myGrid = new Grid();
+        this.KeyDown += MainWindow_KeyDown;
+        myGrid = new Grid();
         GridInit(myGrid);
 
         int[] startPosition = randomPosition();
@@ -29,8 +32,74 @@ public partial class MainWindow : Window
         GetTextBlock(startPosition[1], startPosition[3], 
             startPosition[5].ToString(), myGrid);
         this.Content = myGrid;
+        int[,] matrix2048 = Get2048Matrix();
+        moveArrayRight(matrix2048);
+    }
+    
+    private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Left)
+        {
+            MessageBox.Show("Ai apăsat săgeata stânga!");
+        }
+        else if (e.Key == Key.Right)
+        {
+            MessageBox.Show("Ai apăsat săgeata dreapta!");
+        }
+        else if (e.Key == Key.Up)
+        {
+            MessageBox.Show("Ai apăsat săgeata sus!");
+        }
+        else if (e.Key == Key.Down)
+        {
+            MessageBox.Show("Ai apăsat săgeata jos!");
+        }
     }
 
+    private static void moveArrayRight(int[,] array)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            
+        }
+    }
+
+    private static string getValueFromCell(int i, int j)
+    {
+        foreach (TextBlock textBlock in myGrid.Children)
+        {
+            if (Grid.GetRow(textBlock) == i && Grid.GetColumn(textBlock) == j)
+            {
+                return textBlock.Text;
+            }
+        }
+
+        return ""
+            ;
+    }
+
+    private static int[,] Get2048Matrix()
+    {
+        int[,] matrix2048 = new int[4, 4];
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                string value = getValueFromCell(i, j);
+                if (value.Equals(String.Empty))
+                {
+                    matrix2048[i, j] = 0;
+                }
+                else
+                {
+                    matrix2048[i, j] = int.Parse(getValueFromCell(i, j));
+                }
+            }
+        }
+
+        return matrix2048;
+    }
+    
     private static void GridInit(Grid myGrid)
     {
         myGrid.Width = 250;
@@ -50,6 +119,36 @@ public partial class MainWindow : Window
             RowDefinition rowDef = new RowDefinition();
             myGrid.RowDefinitions.Add(rowDef);
         }
+    }
+
+    int[] randomCoordinatesAndNumber()
+    {
+       
+            var columnPosition1 = new Random().Next(4);
+            var rowPosition1 = new Random().Next(4);
+        
+            var columnPosition2 = new Random().Next(4);
+            var rowPosition2 = new Random().Next(4);
+            while (columnPosition1 == columnPosition2 && rowPosition1 == rowPosition2)
+            {
+                columnPosition2 = new Random().Next(4);
+                rowPosition2 = new Random().Next(4);
+            }
+
+            var oddsOfGetting2or4 = Get10PercentOddsFor4InStartingPosition();
+            var randomPosition1 = new Random().Next(100);
+            var randomPosition2 = new Random().Next(100);
+        
+            while (randomPosition1 == randomPosition2)
+            {
+                randomPosition2 = new Random().Next(100);
+            }
+        
+            return new int[] { columnPosition1, columnPosition2, 
+                rowPosition1, rowPosition2, 
+                oddsOfGetting2or4[randomPosition1],
+                oddsOfGetting2or4[randomPosition2] };
+     
     }
 
     private static void GetTextBlock(int i, int j, String numberInPosition, Grid myGrid)
